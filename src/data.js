d94b15bbe2145b111158cef27dd2942a52fad815
Joao Santos <ctw01895@criticaltechworks.com>
2022-06-20 14:57:00 +0100

    Adding the gifs and pngs.
    Still missing the minishell and fast-food ones.

--- src/data.js
+++ src/data.js
@@ -1,3 +1,14 @@
+// import fast_food_png from './res/fast_food.png'
+// import fast_food_gif from './res/fast_food.gif'
+import minishell_png from './res/minishell.png'
+import minishell_gif from './res/minishell.gif'
+import philo_png from './res/philo.png'
+import philo_gif from './res/philo.gif'
+import cub3d_png from './res/cub3d.png'
+import cub3d_gif from './res/cub3d.gif'
+import so_long_png from './res/so_long.png'
+import so_long_gif from './res/so_long.gif'
+
 export const personalData = {
     name: 'Joao',
     role: 'Front-End Developer'
@@ -16,8 +27,8 @@ export const projects = [
     },
     {
         title: "Minishell",
-        static: "url1",
-        gif: "url2",
+        static: minishell_png,
+        gif: minishell_gif,
         brief: "",
         languages: ["C"],
         keywords: ["linux", "unix", "shell"],
@@ -27,35 +38,35 @@ export const projects = [
     },
     {
         title: "Cub3D",
-        static: "url1",
-        gif: "url2",
-        brief: "",
+        static: cub3d_png,
+        gif: cub3d_gif,
+        brief: "Simple 3D maze using raycasting",
         languages: ["C"],
         keywords: ["game development", "graphics", "3d"],
-        repo: "",
+        repo: "https://github.com/jsantos42/cub3d",
         live: "",
         description: "Small shell sadjfsdfjafsadfaj"
     },
     {
         title: "So Long",
-        static: "url1",
-        gif: "url2",
-        brief: "",
+        static: so_long_png,
+        gif: so_long_gif,
+        brief: "Simple 2D game, using textures and sprites",
         languages: ["C"],
         keywords: ["game development", "graphics", "2d"],
-        repo: "",
+        repo: "https://github.com/jsantos42/so_long",
         live: "",
-        description: "Small shell sadjfsdfjafsadfaj"
+        description: ""
     },
     {
-        title: "Other",
-        static: "url1",
-        gif: "url2",
-        brief: "",
-        languages: ["C"],
-        keywords: ["Linux", "Unix"],
-        repo: "",
+        title: "Dining Philosophers",
+        static: philo_png,
+        gif: philo_gif,
+        brief: "Practical implementation of a concurrent algorithm, using thread, using threads. Done both in C and C++",
+        languages: ["C", "C++"],
+        keywords: ["concurrency", "threads", "unix"],
+        repo: "https://github.com/jsantos42/Dining_Philosophers",
         live: "",
-        description: "Small shell sadjfsdfjafsadfaj"
-    },
+        description: ""
+    }
 ]
