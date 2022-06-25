d397866d3baef6ee8ebf70dcf8a60c9c360af2cd
jsantos <jsantos@student.42lisboa.com>
2022-06-25 20:21:49 +0100

    Project page now fully responsive.

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -3,43 +3,45 @@ import './ProjectCard.css'
 
 
 class ProjectCard extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {imgSrc: this.props.static}
-		this.switchToGif = this.switchToGif.bind(this);
-		this.switchToStatic = this.switchToStatic.bind(this);
-		this.insertLive = this.insertLive.bind(this);
-	}
+    constructor(props) {
+        super(props);
+        this.state = {imgSrc: this.props.static}
+        this.switchToGif = this.switchToGif.bind(this);
+        this.switchToStatic = this.switchToStatic.bind(this);
+        this.insertLive = this.insertLive.bind(this);
+    }
 
-	switchToGif() {
-		this.setState({imgSrc: this.props.gif})
-	}
+    switchToGif() {
+        this.setState({imgSrc: this.props.gif})
+    }
 
-	switchToStatic() {
-		this.setState({imgSrc: this.props.static})
-	}
+    switchToStatic() {
+        this.setState({imgSrc: this.props.static})
+    }
 
-	insertLive() {
-		if (this.props.live.length !== 0)
-			return (
-				 <span> | <a href={this.props.live}>Live</a></span>
-			);
-	}
+    insertLive() {
+        if (this.props.live.length !== 0)
+            return (
+                <span> | <a href={this.props.live}>Live</a></span>
+            );
+    }
 
-	render() {
-		return (
-			<div className='projectCard'>
-				<h2>{this.props.title}</h2>
-				<div>
-					<p><strong>{this.props.brief}</strong></p>
-					<p>Languages: <strong>{this.props.languages.join(', ')}</strong></p>
-					<p>Keywords: <em>{this.props.keywords.join(', ')}</em></p>
-					<p className='links'><a href={this.props.repo}>Repository</a>{this.insertLive()}</p>
-				</div>
-				<img src={this.state.imgSrc} onMouseEnter={this.switchToGif} onMouseLeave={this.switchToStatic}/>
-			</div>
-		)
-	}
+    render() {
+        return (
+            <div className='projectCard'>
+                <h2>{this.props.title}</h2>
+                <div className='textAndImage'>
+                    <img src={this.state.imgSrc} onMouseEnter={this.switchToGif} onMouseLeave={this.switchToStatic}/>
+                    <div className='text'>
+                        <p><strong>{this.props.brief}</strong></p>
+                        <p>Languages: <strong>{this.props.languages.join(', ')}</strong></p>
+                        <p>Keywords: <em>{this.props.keywords.join(', ')}</em></p>
+                        <p className='links'><a href={this.props.repo}>Repository</a>{this.insertLive()}</p>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default ProjectCard;
