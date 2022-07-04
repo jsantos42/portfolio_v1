857285e3d37a8190d426dc248ef3fd58fc42d163
jsantos <jsantos@student.42lisboa.com>
2022-07-04 14:29:33 +0100

    Doing Homepage.

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,9 @@
 import "./Home.css"
 import {Component} from "react";
 import {personalData} from "../data";
-import MainAvatar from "../res/undraw_shared_workspace_re_3gsu.png"
+import Programmer from "../res/undraw_shared_workspace_re_3gsu.png"
+import Doctor from "../res/undraw_doctor_kw5l.png"
+import Highlight from "../components/Highlight";
 
 const {name, currentRole, newRole} = personalData;
 
@@ -9,11 +11,27 @@ class Home extends Component {
     render() {
         return (
             <div className='home'>
-                <em><h2>(As you can see, this homepage is still in construction!</h2>
-                <h2>But you can check the Projects one, or download my resume)</h2></em>
-                <h1>Hello!</h1>
-                <img src={MainAvatar}/>
-                <h1>I'm a {currentRole} with a strong interest in Frontend.</h1>
+                {/*<div style={{width: '100vw'}}>*/}
+                <h1>Software Engineer <br/>keen on Frontend</h1>
+                {/*</div>*/}
+                <img src={Programmer}/>
+                <div className='homeContent'>
+                    <h2>About Me:</h2>
+                    <ul>
+                        <li>background in <Highlight>Medicine</Highlight></li>
+                        <li><Highlight>6+ years</Highlight> of clinical practice</li>
+                        <li>worked in <Highlight>2 countries</Highlight> (PT and FR)</li>
+                        <li>frustrated by the <Highlight>poor UX</Highlight> of healthcare software</li>
+                        <li>decided to <Highlight>change</Highlight> that, switched to programming</li>
+                        <li>long term <Highlight>goal</Highlight>: help rebuilding our NHS software</li>
+                        <li>for now, willing to work on <Highlight>any</Highlight> field</li>
+                        <li>highly <Highlight>curious</Highlight> and <Highlight>motivated</Highlight> individual</li>
+                        <li>sensitive to <Highlight>UX</Highlight> and <Highlight>UI</Highlight> design/aesthetics</li>
+                        <li>worships <Highlight>Sir Jony Ive</Highlight></li>
+                    </ul>
+                    <h2>Want to know more?</h2>
+                    <button>Contact Me</button>
+                </div>
             </div>
         )
     }
