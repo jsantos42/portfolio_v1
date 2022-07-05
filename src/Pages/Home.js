02676d9b09763a4f5b31c205c6b8eef1f7e45b24
jsantos <jsantos@student.42lisboa.com>
2022-07-05 17:01:34 +0100

    Improving the first page.

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,6 +4,7 @@ import {personalData} from "../data";
 import Programmer from "../res/undraw_shared_workspace_re_3gsu.png"
 import Doctor from "../res/undraw_doctor_kw5l.png"
 import Highlight from "../components/Highlight";
+import Me from "../res/me.png"
 
 const {name, currentRole, newRole} = personalData;
 
@@ -26,11 +27,13 @@ class Home extends Component {
                         <li>for now, willing to work on <Highlight>any</Highlight> field</li>
                         <li>highly <Highlight>curious</Highlight> and <Highlight>motivated</Highlight> individual</li>
                         <li>sensitive to <Highlight>UX</Highlight> and <Highlight>UI</Highlight> design/aesthetics</li>
-                        <li>worships <Highlight>Sir Jony Ive</Highlight></li>
+                        <li>worship <Highlight>Sir Jony Ive</Highlight></li>
                     </ul>
                 </div>
                 <div className='more'>
-                    <h2>Want to know more?</h2>
+                {/*    <h2>Interested?</h2>*/}
+                {/*    <h2>Contact me!</h2>*/}
+                {/*    <img src={Me}/>*/}
                     <button>Contact Me (this button still doesn't work!)</button>
                 </div>
             </div>
