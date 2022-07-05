b7eb303fa6f1be34d3603a64baf1e5eda15812fa
jsantos <jsantos@student.42lisboa.com>
2022-07-05 17:00:14 +0100

    Icons on the footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,21 @@
 import {Component} from "react";
 import "./Footer.css"
+import Social from "./Social";
+import LinkedIn from "../res/linked_in.png";
+import Git from "../res/git.png";
+import Email from "../res/email.png";
+import A from './A'
 
 class Footer extends Component {
     render() {
         return (
             <footer>
-                <ul>
-                    <li>© {new Date().getFullYear()} João Almeida Santos</li>
-                </ul>
+                <Social>
+                    <A href={'https://www.linkedin.com/in/j-almeidasantos/'}><img src={LinkedIn}/></A>
+                    <A href={'https://github.com/jsantos42'}><img src={Git}/></A>
+                    <A href={'mailto:j-almeidasantos@hotmail.com'}><img src={Email}/></A>
+                </Social>
+                <p>© {new Date().getFullYear()} João Almeida Santos</p>
             </footer>
         );
     }
---
+++ src/components/Social.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Social = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 1.5rem;
+  padding: 0 0 1rem 0;
+  & img:hover {
+    opacity: 50%;
+  }
+`
+
+export default Social;
