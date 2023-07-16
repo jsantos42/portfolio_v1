7bda2ca642d93a92b0f2e0cff938805483c02eb7
João Almeida Santos <79864209+jsantos42@users.noreply.github.com>
2023-07-16 15:58:25 +0200

    feat: add stack

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,6 +7,21 @@ import Programmer from "../res/undraw_shared_workspace_re_3gsu.png"
 import styled from "styled-components";
 // import Doctor from "../res/undraw_doctor_kw5l.png"
 // import Me from "../res/me.png"
+import js from "../res/stack_logos/js.png"
+import react from "../res/stack_logos/react.png"
+import ember from "../res/stack_logos/ember.png"
+import nodejs from "../res/stack_logos/nodejs.png"
+import express from "../res/stack_logos/express.png"
+import nextjs from "../res/stack_logos/nextjs.png"
+import php from "../res/stack_logos/php.png"
+import laravel from "../res/stack_logos/laravel.png"
+import python from "../res/stack_logos/python.png"
+import r from "../res/stack_logos/r.png"
+import c from "../res/stack_logos/c.png"
+import cpp from "../res/stack_logos/cpp.png"
+import docker from "../res/stack_logos/docker.png"
+import mysql from "../res/stack_logos/mysql.png"
+import postgresql from "../res/stack_logos/postgresql.png"
 
 //==============================================================================
 // STYLING
@@ -70,6 +85,19 @@ export const About = styled.div`
   }
 `
 
+export const Stack = styled.div`
+  display: grid;
+  align-items: center;
+  justify-items: center;
+  grid-template-columns: repeat(auto-fill, minmax(80px, 1fr));
+  gap: 5rem;
+  width: 100%;
+  
+  img {
+	width: 80px;
+  }
+`
+
 export const More = styled.div`
   display: flex;
   align-items: center;
@@ -125,6 +153,24 @@ const Home = () => {
 					<li>working @<Highlight>haelsi</Highlight></li>
 					<li>improving an <Highlight>Electronic Medical Record (EMR)</Highlight> software</li>
                 </ul>
+				<h2>Stack</h2>
+				<Stack>
+					<img src={js} alt={'js'}/>
+					<img src={react} alt={'react'}/>
+					<img src={ember} alt={'ember'}/>
+					<img src={nodejs} alt={'nodejs'}/>
+					<img src={express} alt={'express'}/>
+					<img src={nextjs} alt={'nextjs'}/>
+					<img src={php} alt={'php'}/>
+					<img src={laravel} alt={'laravel'}/>
+					<img src={python} alt={'python'}/>
+					<img src={r} alt={'r'}/>
+					<img src={c} alt={'c'}/>
+					<img src={cpp} alt={'cpp'}/>
+					<img src={docker} alt={'docker'}/>
+					<img src={mysql} alt={'mysql'}/>
+					<img src={postgresql} alt={'postgresql'}/>
+				</Stack>
             </About>
         </StyledHome>
     )
